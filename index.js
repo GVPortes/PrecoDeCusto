e101b2a4957d3e7a5481ec78b979ac3718c8254f
Gabriel Portes <152006861+GVPortes@users.noreply.github.com>
2024-01-08 15:32:50 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let pao = 0.8
+let bife = 1.0
+let ovo = 0.7
+let batataSaladaEmbalagem = 2.5
+let presunto = 1.5
+let mussarela = 2.0
+let BCF = 1.5
+let hamburguer = pao + bife + ovo + batataSaladaEmbalagem
+let xEggBurger = hamburguer + presunto + mussarela
+let xEggBCF = hamburguer + presunto + mussarela + BCF
+ console.log ("O preço de custo do Hamburguer é " + hamburguer)
+ console.log ("O preço de custo do X Egg Burger é " + xEggBurger)
+ console.log ("O preço de custo do X Egg Bacon, frango e calabresa é " + xEggBCF)
